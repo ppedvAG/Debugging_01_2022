a05f378bb595ad376a630ba360cd7d6aba55a8c9
André <ruland@outlook.de>
2022-01-25 14:41:11 +0100

    Hallo Debugging

---
+++ HalloDebugger/HalloDebugger/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using TolleLib;
+
+namespace HalloDebugger
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+#if DEBUG
+            Console.WriteLine("DEBUG MODE");
+#elif Wurst
+            Console.WriteLine("Wurst MODE");
+    
+#else
+            Console.WriteLine("RELEASE MODE");
+#endif
+
+
+            Console.WriteLine("Hallo Debugger");
+            Console.WriteLine("Pause");
+            Console.ReadLine();
+            MachSachen();
+
+
+            var c1 = new Class1();
+            c1.SagHallo();
+
+            Console.ReadKey();
+        }
+
+        private static void MachSachen()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ZeigeText($"Hallo Welt {i}");
+            }
+
+        }
+
+        private static void ZeigeText(string v)
+        {
+            Console.WriteLine(v);
+
+
+            if (v.Contains("8"))
+            {
+                Console.WriteLine("Press 4 Error");
+                Console.ReadLine();
+                throw new ExecutionEngineException();
+            }
+
+        }
+    }
+}
---
+++ TolleLib/TolleLib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TolleLib
+{
+    public class Class1
+    {
+
+        public int Zahl { get; set; } = 16;
+        public void SagHallo()
+        {
+            Console.WriteLine("Hallo von der Besten Lib222");
+        }
+    }
+}
