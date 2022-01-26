9dbdf42529bcb6421af0cfb9be6b05d52ecc0e27
André <AndreR@ppedv.de>
2022-01-26 11:34:25 +0100

    Unittests beispiel

---
+++ Calculator/Calculator/Calc.cs
@@ -0,0 +1,13 @@
+﻿namespace Calculator
+{
+    public class Calc
+    {
+        public int Sum(int a, int b)
+        {
+            if (b > 100)
+                return 1100;
+
+            return checked(a + b);
+        }
+    }
+}
